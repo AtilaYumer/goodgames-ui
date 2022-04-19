f0b233a85c4595ac922556d7d9211e3a5ee91ec3
Atila Yumer <atillayumer@gmail.com>
2022-04-20 00:37:16 +0300

    heroku configuration

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/goodgames-ui'));
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname +
-        '/dist/goodgames-ui/index.html'));
-});
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/goodgames-ui'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/goodgames-ui/'}),
+);
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
